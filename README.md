196d01bfdbae87412a1a0e9d7ffd97bd3cf58267
bapu <janmejayaswain14@gmail.com>
2018-12-24 19:23:33 +0530

    added git_demo to local repo

---
+++ README.md
@@ -0,0 +1,1 @@
+## git_demo Java Project
---
+++ src/main/java/com/stackspace/demo/git_demo/Application.java
@@ -0,0 +1,13 @@
+package com.stackspace.demo.git_demo;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git_demo");
+    }
+    public void addData(String data){
+    	System.out.println(data);
+    	
+    }
+
+}
